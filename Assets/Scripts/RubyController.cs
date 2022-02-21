90614d35a6ad417d4045ee76e6e514fff4bdd567
猫猫秋小裤 <qiuxiaoku@gmail.com>
2022-02-21 23:30:19 +0800

    0221

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,26 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody;
+
+    private float horizontal;
+
+    private float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
         // QualitySettings.vSyncCount = 0;
         // Application.targetFrameRate = 10;
 
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
         // Debug.Log(horizontal);
+    }
+
+    private void FixedUpdate()
+    {
         Vector2 position = transform.position;
-        position.x = position.x + 3f*horizontal*Time.deltaTime;
-        position.y = position.y + 3f * vertical*Time.deltaTime;
-        transform.position = position;
+        position.x = position.x + 3f * horizontal * Time.deltaTime;
+        position.y = position.y + 3f * vertical * Time.deltaTime;
+        _rigidbody.MovePosition(position);
     }
 }
